30dcd49bd2bc7019b8781aa69e60e38d09980924
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-01-23 22:14:57 +0100

    Initial Commit

---
+++ mobileCashierApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobileCashierApp
+//
+//  Created by Shafigh Khalili on 2020-01-23.
+//  Copyright © 2020 Shafigh Khalili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
